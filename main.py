8477cf6b06dbc6b5679abb7854bc187241e26413
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-01-11 00:34:50 +0530

    Made changes

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# In this script, we first import the tweepy library, which is used to interact with the Twitter API, and the time library, which is used to schedule the tweets. Next, we authenticate with the Twitter API using our API key, API secret, access token, and access token secret. Then, we define the number of tweets to be posted and the interval between them (in seconds).
+# The get_tweets function uses the user_timeline method of the Tweepy API to get the 10 most recent tweets from the specified user. The post_tweets function then iterates through these tweets and uses the update_status method to post them to the authenticated account's timeline.
 
-# Then, we get the current time and enter a loop that will run for the number of tweets we want to post. Within the loop, we use the api.update_status() method to post a tweet. We have used "This is tweet #{}".format(i+1) as a text for tweet, you can change it with whatever you want. In addition, you can also add an image or video using the api.update_with_media() method.
-
-# After each tweet, the script will sleep for interval seconds. Once all tweets have been posted, the script will calculate the elapsed time and print a message indicating that all tweets have been posted.
-
+# schedule_tweets uses the schedule library to schedule the post_tweets function to run every 2 minutes. The script also has a infinite loop that calls schedule's run_pending method which runs any scheduled tasks that are due to be run and then sleeps for 1 sec.
 
 import base64
 import hashlib
@@ -19,29 +16,64 @@ import time
 import schedule
 import tweepy
 import twitter
+from tweepy import API
+from tweepy import Cursor
+from tweepy.errors import TweepyException
+from tweepy import OAuthHandler
 
+# Authenticate to Twitter
 auth = tweepy.OAuthHandler("iB9LQqxubKW7wvLKAyltZgaOZ", "PORmKfASEYF9buYginDQzNxw0h9mf6L5QiuzrZW2Ncfsa9k7gW")
 auth.set_access_token("1568647627075629056-YGXlgpvaTO4EC1lMkhJ0IxKC49wYgE", "hGReqICstQv0Fvvb9aKyww6BO7mLNroxbyF8HXWBuRdYU")
-api = tweepy.API("AAAAAAAAAAAAAAAAAAAAAJ%2BJlAEAAAAAU0leZ2ViAf03%2BmwuimxaNnkuhiQ%3D7oHCRnQz0F14UgYdMfU2nrndbrY2Ztj45esdxIv3tKeGXqFz7M")
+api = tweepy.API(auth)
+
+def refresh_access_token():
+    if api.access_token_expired:
+        api.refresh_access_token()
+
+def get_tweets():
+    # Get tweets from specified user
+    tweets = api.user_timeline(screen_name = "hourlyshitpost2", count = 24)
+    return tweets
+
+def post_tweets():
+    tweets = get_tweets()
+    for tweet in tweets:
+        try:
+            if 'media' in tweet.entities:
+                media_list = tweet.entities['media']
+                for media in media_list:
+                    print(media['media_url'])
+                    #posts meme using media_url
+                    api.update_status(status=tweet.text, media_ids=[media['media_id']])
+            else:
+                api.update_status(tweet.text)
+            print("Tweet posted: " + tweet.text)
+        except tweepy.TweepyException as error:
+            print(error.reason)
+            continue
+
+def schedule_tweets():
+    # Schedule tweets to be posted every hour
+    schedule.every().hour.do(post_tweets)
+    schedule.every().hour.do(refresh_access_token)
+
+if __name__ == "__main__":
+    schedule_tweets()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 # print(api)
-startTime = time.time()
-tweets = api.search_tweets( q = 'funny' )
-for tweet in range(tweets):
-    try:
-        api.update_status(tweet.text)
-        print("Tweet #{} posted!".format(tweet+1))
-    except tweepy.error.TweepError as e:
-        print("Error: {}",format(e))
-    time.sleep(60)
+# startTime = time.time()
+# tweets = api.search_tweets( q = 'funny' )
+# for tweet in range(tweets):
+#     try:
+#         api.update_status(tweet.text)
+#         print("Tweet #{} posted!".format(tweet+1))
+#     except tweepy.error.TweepError as e:
+#         print("Error: {}",format(e))
+#     time.sleep(60)
 
-elapsedTime = time.time() - startTime
+# elapsedTime = time.time() - startTime
 
-print("All tweets have been posted in {} seconds".format(elapsedTime))
-
-# In this script, we first import the tweepy library, which is used to interact with the Twitter API, and the time library, which is used to schedule the tweets. Next, we authenticate with the Twitter API using our API key, API secret, access token, and access token secret. Then, we define the number of tweets to be posted and the interval between them (in seconds).
-
-# Then, we get the current time and enter a loop that will run for the number of tweets we want to post. Within the loop, we use the api.update_status() method to post a tweet. We have used "This is tweet #{}".format(i+1) as a text for tweet, you can change it with whatever you want. In addition, you can also add an image or video using the api.update_with_media() method.
-
-# After each tweet, the script will sleep for interval seconds. Once all tweets have been posted, the script will calculate the elapsed time and print a message indicating that all tweets have been posted.
-
+# print("All tweets have been posted in {} seconds".format(elapsedTime))
